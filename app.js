e3f6d5e68d271e0728f3e52a892d2effa9057230
leehyunjong <ahajongs@gmail.com>
2020-12-09 15:46:31 +0900

    feat:leehyunjong cors모듈 추가

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var corse = require('cors')
 const {sequelize} = require('./models')
 
 sequelize.sync({alter : true})
@@ -21,6 +22,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(corse())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
