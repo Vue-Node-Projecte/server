f2058ef10bb7b6dee497128f16415fa05168ef9e
leehyunjong <ahajongs@gmail.com>
2020-12-15 23:16:49 +0900

    feat:leehyunjong 기간리포트 토큰값수정

--- routes/report/index.js
+++ routes/report/index.js
@@ -9,6 +9,6 @@ router.post('/questionReport',authUtil.checkStudentToken,reportController.setInd
 router.post('/homework/:homeworkId/wordHomeWorkReport',authUtil.checkStudentToken,reportController.setHomeworkWordReport)
 router.post('/homework/:homeworkId/sentenceHomeworkReport',authUtil.checkStudentToken,reportController.setHomeworkSentenceReport)
 router.post('/homework/:homeworkId/questionHomeworkReport',authUtil.checkStudentToken,reportController.setHomeworkQuestionReport)
-router.post('/period',authUtil.checkStudentToken,reportController.getPeriodReport)
+router.post('/period',authUtil.checkToken,reportController.getPeriodReport)
 
 module.exports=router
